5983d686e8a6467557f564427cfdcbc5bb54e72c
Nicholas Worthy <40613355+tworthy94@users.noreply.github.com>
2019-06-25 19:23:14 -0400

    Averaged the full data table

--- ETL_Queries.sql
+++ ETL_Queries.sql
@@ -2,5 +2,8 @@ SELECT * FROM full_data
 
 SELECT * FROM census_data
 
+ALTER TABLE census_data
+RENAME COLUMN "State" TO "state";
+
 SELECT * FROM full_data_by_state
-INNER JOIN census_data on census_data."State" = full_data_by_state."state";
+INNER JOIN census_data ON census_data."state" = full_data_by_state."state";
